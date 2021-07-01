57d8c7010e237e017768a955269747f90f025ecf
kooldan <kooldan1000@gmail.com>
2021-07-01 22:57:49 +0900

    structure rebuild for heroku

--- web/indes.php
+++ web/indes.php
@@ -8,15 +8,12 @@
 <html lang="en">
 
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
   <title>cars for rent</title>
-  <!-- Bootstrap core CSS -->
   <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <!-- Custom styles for this template -->
   <link href="css/shop-homepage.css" rel="stylesheet">
 </head>
 
