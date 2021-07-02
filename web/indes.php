d26b440a68f09d90ec04c88d79bc82c1fd40b41c
kooldan <kooldan1000@gmail.com>
2021-07-02 10:00:25 +0900

    update composer files

--- web/indes.php
+++ web/indes.php
@@ -8,56 +8,10 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <title>cars for rent</title>
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/shop-homepage.css" rel="stylesheet">
+
 </head>
 
 <body>
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div class="container">
-      <a class="navbar-brand" href="#">cars for rent</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          
-          <?php 
-          if($query_user->num_rows == 0){?>
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Главная</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="login.php">Войти</a>
-          </li>
-          <?php }
-          else{ ?>
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php?user=<?php echo $result_user['id']?>">Главная</a>
-          </li>
-          <li class="nav-item">
-          <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <?php echo $result_user["username"] ?>
-            </button>
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
-              <a class="dropdown-item" href="user.php?id=<?php echo $result_user['id']."&user=".$result_user['id'];?>">Аккаунт</a>
-              <a class="dropdown-item text-danger" href="index.php">Выйти</a>
-            </div>
-          </div>
-          </li>
-          <?php } ?>
-        </ul>
-      </div>
-    </div>
-  </nav>
 
   <!-- Page Content -->
   <div class="container">
@@ -138,19 +92,12 @@
     <!-- /.row -->
 
   </div>
-  <!-- /.container -->
-
-  <!-- Footer -->
+  
   <footer class="py-5 bg-dark">
     <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+      <p class="m-0 text-center text-white">Copyright &copy; KoolDan 2021</p>
     </div>
-    <!-- /.container -->
   </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
